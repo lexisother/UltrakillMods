a708cddee25bb473f777859102fad0e01746df8d
Alyxia Sother <alyxia@riseup.net>
2024-09-14 12:08:44 +0200

    [DebugMode] Add

---
+++ DebugMode/Plugin.fs
@@ -0,0 +1,20 @@
+﻿namespace DebugMode
+
+open BepInEx
+open HarmonyLib
+open DebugMode.PluginInfo
+
+[<HarmonyPatch(typeof<UnityEngine.Debug>)>]
+type Patch() =
+    [<HarmonyPatch("isDebugBuild", MethodType.Getter)>]
+    [<HarmonyPostfix>]
+    static member get_isDebugBuild(__result: byref<bool>) =
+        __result <- true
+        ()
+
+[<BepInPlugin(pluginId, pluginName, pluginVersion)>]
+type Plugin() =
+    inherit BaseUnityPlugin()
+
+    member this.Awake() =
+        Harmony(pluginId).PatchAll()
---
+++ DebugMode/PluginInfo.fs
@@ -0,0 +1,5 @@
+﻿module DebugMode.PluginInfo
+
+let [<Literal>] pluginName = "DebugMode"
+let [<Literal>] pluginId = "alyxia." + pluginName
+let [<Literal>] pluginVersion = "1.0.0"
